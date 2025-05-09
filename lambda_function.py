6e5356a07943dec9091a5c22a14ed4ae3448346b
Ashutoshrx <asutosh007satpathy@gmail.com>
2025-05-09 20:00:19 +0530

    working code of fetching records from RDS and pushing to S3

--- lambda_function.py
+++ lambda_function.py
@@ -8,84 +8,99 @@ import psycopg2
 logger=logging.getLogger()
 logger.setLevel(logging.INFO)
 
-def fetch_records():
-    list_of_records = [
-        {'Id': 1, 'firstName': 'Ashutosh', 'lastName': 'Satapathy'},
-        {'Id': 2, 'firstName': 'Ronnie', 'lastName': 'Satapathy'},
-        {'Id': 3, 'firstName': 'Kamal', 'lastName': 'Lochan'},
-        {'Id': 4, 'firstName': 'Sabyasachi', 'lastName': 'Jr.'},
-        {'Id': 5, 'firstName': 'Amitabh', 'lastName': 'Patnaik'},
-        {'Id': 6, 'firstName': 'Nihar', 'lastName': 'Das'},
-        {'Id': 7, 'firstName': 'Sumit', 'lastName': 'Mohanty'},
-        {'Id': 8, 'firstName': 'Prachi', 'lastName': 'Mishra'},
-        {'Id': 9, 'firstName': 'Rashmita', 'lastName': 'Behera'}
-    ]  
-    return list_of_records
 
-def filter_records(records, **criteria):
-    return [record for record in records if all(record.get(key) == value for key, value in criteria.items())]
-
-def connect_to_DB():
+def connect_to_DB(event):
     try:
+        logger.info("Started Connecting to PostgreSQL")
+
         conn = psycopg2.connect(
-            host="policy-poc.cluster-clzfvi53z3dr.us-east-1.rds.amazonaws.com",
-            database="policy-poc",
+            host="proxy-1746712528059-policy-p3.proxy-clzfvi53z3dr.us-east-1.rds.amazonaws.com",
+            database="policy_poc_2",
             user="postgres",
             password="postgres",
             port=5432
         )
+
         logger.info("Connected to PostgreSQL")
 
         cur = conn.cursor()
         query= """
-            CREATE TABLE policy_poc (
-                policyId INT PRIMARY KEY AUTO_INCREMENT,  -- Unique identifier for the policy
-                servicerOrgNumber VARCHAR(50) NOT NULL,   -- Organization number associated with the policy
-                status VARCHAR(20) NOT NULL,              -- Status of the policy (e.g., Active, Pending, Closed)
-                policyName VARCHAR(100),                  -- Name of the policy
-                createdDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  -- Timestamp when record was created
-                updatedDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP -- Last updated timestamp
-            );
+            SELECT * FROM policy_search_load_test WHERE 1=1
          """
+        if event.get('billingOrgNumber'):
+            query += " AND servicer_org_nbr = '%s'"%event.get("billingOrgNumber")
+
         cur.execute(query)
-        # rows = cur.fetchall()
-        # logger.info(f"Fetched {len(rows)} rows")
+        result = cur.fetchall()
+        logger.info(f"Final query prepared as: {query}")
+        logger.info(f"Fetched {len(result)} rows")
 
         cur.close()
         conn.close()
+        
+        return result
 
-        return {
-            "statusCode": 200,
-            "body": rows  # You might need to serialize rows to JSON
-        }
 
     except Exception as e:
         raise e
+
+def build_response(statusCode, body=None):
+    response = {
+        'statusCode': statusCode,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+    if body:
+        response['body'] = body
+    return response
+
+
+def prepare_response(result):
+    try:
+        logger.info("Started preparing response")
+        column_names = ["policyNumber", "billingOrgNumber", "status"]
+        json_ready = [
+        {key: value for key, value in zip(column_names, row[:3])}
+        for row in result
+    ]
+        logger.info(f"Successfully prepared response: {json_ready[0]}")
+        return json_ready
+    except Exception as e:
+        logger.error(f"Something went wrong {e}")
+        return build_response(500, json.dumps({"error": str(e)}))
 
 
 s3=boto3.client('s3')
 def lambda_handler(event, context):
     try:
-        connect_to_DB()
-        # logger.info(f"Input Event is {json.dumps(event)}")
-        # logger.info("Started uploading file to S3")
-        # currentDateTime=datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
-        # logger.info(f"Successfully added Date time {currentDateTime}")
-        # bucket='enact-prac-sqs'
-        # result = fetch_records()
+        logger.info("Started execution")
+        result=connect_to_DB(event)
+        
+        logger.info(f"Input Event is {json.dumps(event)}")
+        billingOrgNumber=event.get('billingOrgNumber')
+        logger.info(f"BillingOrgNumber is {billingOrgNumber}")
+        logger.info("Started uploading file to S3")
+        currentDateTime=datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+        logger.info(f"Successfully added Date time {currentDateTime}")
+        bucket='enact-prac-sqs'
         # logger.info(f"Result is {result}")
-        # logger.info(f"Criteria is {json.dumps(event)}")
-        # fileToUpload = filter_records(result, **event)
-
-        # logger.info(f"Filtering completed {fileToUpload}")
-        # logger.info("Filtering completed")
-        # fileName='Ronnie'+ str(currentDateTime) +  '.json'
-        # logger.info(f"File Name is ',{fileName}")
-        # uploadByteStream=bytes(json.dumps(fileToUpload).encode('UTF-8'))
-        # s3.put_object(Bucket=bucket,Key='TEMP/'+fileName,Body=uploadByteStream)
-        # logger.info('Upload Completed Successfully')
+        json_array=prepare_response(result)
+        # json_array = [r[0] for r in result]
+        logger.info(f"First Index of array is {json_array[0]}")
+        
+        fileName='policies'+ str(currentDateTime) +  '.json'
+        logger.info(f"File Name is ',{fileName}")
+        key='TEMP/'+fileName
+        uploadByteStream=bytes(json.dumps(json_array).encode('UTF-8'))
+        logger.info(f"Testing: {json_array[0]}")
+        logger.info(f"Started uploading the file to S3 bucket {bucket} and path is {key} of size {len(json_array)}")
+        s3.put_object(Bucket=bucket,Key=key,Body=uploadByteStream)
+        logger.info('Upload Completed Successfully')
+        finalRes={"bucket":bucket,"key":key}
+        return build_response(200, json.dumps(finalRes))
 
     except Exception as e:
         logger.error(f"Something went wrong {e}")
-        raise e
-    
+        return build_response(500, json.dumps({"error": str(e)}))
